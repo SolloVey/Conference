24ba70575dd9d0e93c75bf1fa473a83cfc932d3d
SolloVey <chelnokow@gmail.com>
2023-12-01 20:19:54 +0300

    Add adaptive

--- src/index.js
+++ src/index.js
@@ -1,7 +1,99 @@
-// **************************************************
-// *   import 'normalize.css' дописать в json.файл  *
-// *   "dependencies": {                            *
-// *    "normalize.css": "^8.0.1"                   *
-// *   }                                            *
-// **************************************************
+import './styles/reset.css';
+import './styles/fonts/fonts.css';
 import './styles/main.scss';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 2,
+	spaceBetween: 40,
+	slidesPerGroup: 1,
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	modules: [Navigation, Pagination],
+	breakpoints: {
+		320: {
+			spaceBetween: 38,
+			slidesPerView: 1,
+			pagination: {
+				dynamicBullets: true,
+			},
+		},
+		701: {
+			slidesPerView: 2,
+		},
+		769: {
+			spaceBetween: 40,
+		},
+	},
+});
+
+const swiperPrev = document.querySelector('#swiperPrev');
+const swiperNext = document.querySelector('#swiperNext');
+const linksMenu = document.querySelectorAll(
+	'.nav__list__item__link[data-goto]'
+);
+const body = document.querySelector('body');
+const btnBurger = document.querySelector('.header__burger');
+const navList = document.querySelector('.nav');
+const navBtn = document.querySelector('.nav__btn');
+
+// Плавный переход к разделу по клику в меню
+if (linksMenu.length > 0) {
+	linksMenu.forEach(element => {
+		element.addEventListener('click', onMenuLinkClick);
+	});
+}
+
+function onMenuLinkClick(event) {
+	event.preventDefault();
+
+	const linkMenu = event.target;
+	const gotoBlock = document.querySelector(linkMenu.dataset.goto);
+
+	// if (linkMenu.dataset.goto && gotoBlock) {
+	if (linkMenu.dataset && gotoBlock) {
+		const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY;
+
+		closeBurgerMenu();
+
+		window.scrollTo({
+			top: gotoBlockValue,
+			behavior: 'smooth',
+		});
+	}
+}
+
+// Показать меню БУРГЕР
+function showBurgerMenu(event) {
+	event.preventDefault();
+	body.classList.toggle('lock');
+	btnBurger.classList.toggle('active');
+	navList.classList.toggle('active');
+}
+
+// Убрать меню БУРГЕР
+function closeBurgerMenu() {
+	if (btnBurger.classList.contains('active')) {
+		body.classList.remove('lock');
+		btnBurger.classList.remove('active');
+		navList.classList.remove('active');
+	}
+}
+
+// Вынос навигации слайдера за пределы контейнера Swiper
+swiperPrev.addEventListener('click', () => swiper.slidePrev());
+swiperNext.addEventListener('click', () => swiper.slideNext());
+btnBurger.addEventListener('click', showBurgerMenu);
+navBtn.addEventListener('click', onMenuLinkClick);
